71f7c5c6bd482b0c5c11e83e2b2c6779110c1cca
mohammadreza <shahbaziweb@gmail.com>
2024-06-01 18:53:04 +0330

    create separate providers

---
+++ src/components/Providers.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@theme/theme";
+import { Loading } from "./Loading";
+import { Toast } from "./Toast";
+import { WithChildren } from "@models/common";
+import '@locales/i18n';
+import { RouterProvider } from "react-router-dom";
+import { router } from "@root/router";
+
+export const Providers = ({children}: WithChildren) => {
+  const queryClient = new QueryClient();
+
+  return (
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <RouterProvider router={router}/>
+          <Loading/>
+          <Toast/>
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,14 @@
-import { Outlet, RouterProvider } from "react-router-dom";
-import { router } from "@root/router";
-import { ThemeProvider } from "@theme/theme";
+import { Outlet } from "react-router-dom";
 import '@locales/i18n';
-import { Loading } from "@components/Loading";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toast } from "@components/Toast.tsx";
+import { QueryClient } from '@tanstack/react-query';
+import { Providers } from "@components/Providers";
 
 const queryClient = new QueryClient();
 
 export const App = () => {
   return (
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <RouterProvider router={router}/>
-          <Outlet/>
-          <Loading/>
-          <Toast/>
-        </ThemeProvider>
-      </QueryClientProvider>
+      <Providers>
+        <Outlet/>
+      </Providers>
   )
 }
